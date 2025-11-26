eb6d69604cf3c185d104734ab3e8be2038d7da9e
Gog_2 <gog100500@gmail.com>
2025-11-26 21:41:14 +0300

    Update BubbleSort.cs

--- Assets/MyAssets/Script/Cycles/BubbleSort.cs
+++ Assets/MyAssets/Script/Cycles/BubbleSort.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class BubbleSort : MonoBehaviour
 {
    [SerializeField] private List<GameObject> _bubbleSort;
+   [SerializeField] private float _spacing = 0.5f;
+   
    public void SortBubbleButton()
    {
       SortBubble();
@@ -13,35 +15,44 @@ public class BubbleSort : MonoBehaviour
    {
       bool swapped;
       int i, j;
-      for (i = 0; i < _bubbleSort.Count - 1; i++) {
+      for (i = 0; i < _bubbleSort.Count - 1; i++) 
+      {
          swapped = false;
-         for (j = 0; j < _bubbleSort.Count - i - 1; j++) {
-            if (_bubbleSort[j].transform.position.x +_bubbleSort[j].transform.position.y > _bubbleSort[j + 1].transform.position.x + _bubbleSort[j + 1].transform.position.y) {
-               
-               // ReSharper disable once SwapViaDeconstruction
-               var temp = _bubbleSort[j].transform.position;
-               _bubbleSort[j].transform.position = _bubbleSort[j + 1].transform.position;
-               _bubbleSort[j + 1].transform.position = temp;
+         for (j = 0; j < _bubbleSort.Count - i - 1; j++) 
+         {
+            float currentSize = GetCubeSize(_bubbleSort[j]);
+            float nextSize = GetCubeSize(_bubbleSort[j + 1]);
+            if (currentSize < nextSize) 
+            {
+               GameObject temp = _bubbleSort[j];
+               _bubbleSort[j] = _bubbleSort[j + 1];
+               _bubbleSort[j + 1] = temp;
                swapped = true;
             }
          }
          if (swapped == false)
             break;
       }
-      for (i = 0; i < _bubbleSort.Count - 1; i++) {
-         swapped = false;
-         for (j = 0; j < _bubbleSort.Count - i - 1; j++) {
-            if (_bubbleSort[j].transform.localScale.x +_bubbleSort[j].transform.localScale.y > _bubbleSort[j + 1].transform.localScale.x + _bubbleSort[j + 1].transform.localScale.y) {
-               
-               // ReSharper disable once SwapViaDeconstruction
-               var temp = _bubbleSort[j].transform.localScale;
-               _bubbleSort[j].transform.localScale = _bubbleSort[j + 1].transform.localScale;
-               _bubbleSort[j + 1].transform.localScale = temp;
-               swapped = true;
-            }
-         }
-         if (swapped == false)
-            break;
+      float totalWidth = 0f;
+      for (i = 0; i < _bubbleSort.Count; i++)
+      {
+         totalWidth += _bubbleSort[i].transform.localScale.x;
+         if (i < _bubbleSort.Count - 1)
+            totalWidth += _spacing;
       }
+      float startX = -totalWidth / 2f;
+      float currentX = startX;
+      for (i = 0; i < _bubbleSort.Count; i++)
+      {
+         float cubeHalfWidth = _bubbleSort[i].transform.localScale.x / 2f;
+         Vector3 newPosition = new Vector3(currentX + cubeHalfWidth, 0f, 0f);
+         _bubbleSort[i].transform.position = newPosition;
+         currentX += _bubbleSort[i].transform.localScale.x + _spacing;
+      }
+   }
+   private float GetCubeSize(GameObject cube)
+   {
+      Vector3 scale = cube.transform.localScale;
+      return scale.x * scale.y * scale.z;
    }
 }
